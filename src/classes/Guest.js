92e297d799caf95314acad65257665b539d3bc21
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-11-09 16:23:44 -0500

    added displayMyrooms function

--- src/classes/Guest.js
+++ src/classes/Guest.js
@@ -1,8 +1,14 @@
 class Guest {
 	constructor(guestData) {
 		this.id = guestData.id || 0 ;
-		this.name = guestData.name
+		this.name = guestData.name;
+		this.futureBookings = [];
+		this.pastBookings = [];
 	} 
+	displayMyRooms(hotelBookings) {
+		const myRooms = hotelBookings.filter((hotelBooking) => hotelBooking.userID === this.id)
+			return myRooms
+	}
 };
 
 
--- test/guest-test.js
+++ test/guest-test.js
@@ -1,13 +1,17 @@
 import { expect } from "chai";
 import guestData from '../data/guest-sample';
+import bookingData from '../data/booking-sample';
 import Guest from '../src/classes/Guest';
 
+
 describe('Guest', function () {
-   let guest1, guest2, guest3;
+   let guest1, guest2, guest3, guest5;
    beforeEach(function (){
     guest1 = new Guest (guestData[0]);
     guest2 = new Guest (guestData[1]);
     guest3 = new Guest (guestData[2]);
+    guest5 = new Guest (guestData[8])
+    
    });
     
    it('should be a function', function() {
@@ -27,5 +31,37 @@ describe('Guest', function () {
    it('should have a name', function () {
     expect(guest3.name).to.equal('Kelvin Schiller')
    });
-})
+   
+  it('should be able to see their rooms', function (){
 
+    expect(guest5.displayMyRooms(bookingData)).to.deep.equal(
+        [
+            {
+           	 "id": "5fwrgu4i7k55hl6sz",
+            	"userID": 9,
+            	"date": "2022/04/22",
+           	 "roomNumber": 15
+            },
+            
+            {
+            	"id": "5fwrgu4i7k55hl6xk",
+            	"userID": 9,
+            	"date": "2022/01/31",
+            	"roomNumber": 11
+            }
+        ]
+    	)
+  })
+   
+	it('Should be able to store future bookings', function(){
+		expect(guest2.futureBookings).to.deep.equal([])
+	 })
+	 
+	 it('Should be able to store past bookings', function() {
+		expect(guest3.pastBookings).to.deep.equal([])
+	 })
+
+        
+    
+        
+})
