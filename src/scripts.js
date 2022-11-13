5cbda457b156da9d36789ca2375c647ccc2b5959
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-11-13 13:07:09 -0500

    started QSelects

--- src/scripts.js
+++ src/scripts.js
@@ -10,3 +10,40 @@ import './images/Bed.jpg'
 import './images/pastBed.jpg'
 
 // console.log('This is the JavaScript entry file - your code begins here.');
+
+
+// Query Selectors 
+// navbar area 
+const guestPageBtn = document.querySelector('.customer-page')
+const bookRmPage = document.querySelector('.book-a-room')
+const logOut = document.querySelector('.logOut')
+// Guest Page Query Selectors
+const totalSpent = document.querySelector('.totalSpent')
+const upComingArea = document.querySelector('.card-wrapper-container')
+const pastReservedArea = document.querySelector('.pr-container')
+const wholeGuestPage = document.querySelector('.guestBookingsPage')
+
+
+// global vars
+
+// Api Fetching
+
+// event Listeners 
+guestPageBtn.addEventListener('click', showDashboard)
+bookRmPage.addEventListener('click', showBookingPage)
+logOut.addEventListener('click', showLogOut)
+// Functions
+
+function showPage(page1, page2, page3) {
+    page1.classList.remove(".hidden")
+    page2.classList.add('.hidden')
+    page3.classList.add('hidden')
+
+}
+
+function showDashboard() {
+    showPage(wholeGuestPage)
+}
+function showBookingPage() {
+    showPage()
+}
